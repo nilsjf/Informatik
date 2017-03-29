4a3caaa703d61bd72945798aeb5ff5ccc14a11a2
paul.pietzcker <paul.pietzcker@r210-205.schule.local>
2017-03-29 12:06:59 +0200

    teilen refactor

--- src/TeilenClass.java
+++ src/TeilenClass.java
@@ -10,26 +10,26 @@ public class TeilenClass {
 
 	
 	public static HashMap<String, ArrayList<String>> teilen() {
-		FormelUmsteller.listEingabe.get("eSeite").add(0, "[");
-		FormelUmsteller.listEingabe.get("eSeite").add("]");
-		FormelUmsteller.listEingabe.get("xSeite").add(0, "[");
-		FormelUmsteller.listEingabe.get("xSeite").add("]");
+		FormelUmsteller.listEingabeHM.get("eSeite").add(0, "[");
+		FormelUmsteller.listEingabeHM.get("eSeite").add("]");
+		FormelUmsteller.listEingabeHM.get("xSeite").add(0, "[");
+		FormelUmsteller.listEingabeHM.get("xSeite").add("]");
 		
-		for (int i=0 ; i<FormelUmsteller.listEingabe.get("xSeite").size(); i++) {
-			if (FormelUmsteller.listEingabe.get("xSeite").get(i).equals("x") == false) {
-				FormelUmsteller.zsTeil.add(FormelUmsteller.listEingabe.get("xSeite").get(i));
-				FormelUmsteller.listEingabe.get("xSeite").remove(i);
+		for (int i=0 ; i<FormelUmsteller.listEingabeHM.get("xSeite").size(); i++) {
+			if (FormelUmsteller.listEingabeHM.get("xSeite").get(i).equals("x") == false) {
+				FormelUmsteller.zsTeil.add(FormelUmsteller.listEingabeHM.get("xSeite").get(i));
+				FormelUmsteller.listEingabeHM.get("xSeite").remove(i);
 				i = i-1;
 			}
 		}
 		
-		FormelUmsteller.listEingabe.get("eSeite").add("/");
+		FormelUmsteller.listEingabeHM.get("eSeite").add("/");
 		
 		for (int j=0 ; j < FormelUmsteller.zsTeil.size(); j++){
-			FormelUmsteller.listEingabe.get("eSeite").add(FormelUmsteller.zsTeil.get(j));
+			FormelUmsteller.listEingabeHM.get("eSeite").add(FormelUmsteller.zsTeil.get(j));
 		}
 		
-		return FormelUmsteller.listEingabe;
+		return FormelUmsteller.listEingabeHM;
 	}
 	
 }
